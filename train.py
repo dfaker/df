bdef8345507a94a8da7ab1f27b845e2cb308408d
dfaker <35278260+dfaker@users.noreply.github.com>
2018-01-09 23:36:37 +0000

    Add files via upload

---
+++ train.py
@@ -0,0 +1,143 @@
+import cv2
+import numpy
+
+from utils import get_image_paths, load_images, stack_images
+from training_data import get_training_data
+import glob
+
+import random
+
+from scipy.stats import linregress
+from tqdm import tqdm
+
+if __name__ == '__main__':
+
+  print('running')
+
+  images_A = get_image_paths( "data/A" )
+  images_B = get_image_paths( "data/B"  )
+
+  minImages = min(len(images_A),len(images_B))*2
+
+  random.shuffle(images_A)
+  random.shuffle(images_B)
+
+  images_A,landmarks_A = load_images( images_A[:minImages] ) 
+  images_B,landmarks_B = load_images( images_B[:minImages] )
+
+  print('Images A', images_A.shape)
+  print('Images B', images_B.shape)
+
+  images_A = images_A/255.0
+  images_B = images_B/255.0
+
+  images_A[:,:,:3] += images_B[:,:,:3].mean( axis=(0,1,2) ) - images_A[:,:,:3].mean( axis=(0,1,2) )
+
+  print( "press 'q' to stop training and save model" )
+
+  batch_size = int(64*1.5)
+
+  warped_A, target_A, mask_A = get_training_data( images_A,  landmarks_A,landmarks_B, batch_size )
+  warped_B, target_B, mask_B  = get_training_data( images_B, landmarks_B,landmarks_A, batch_size )
+
+
+  print(warped_A.shape, target_A.shape, mask_A.shape)
+
+  figWarped = numpy.stack([warped_A[:6],warped_B[:6]],axis=0 )
+  figWarped = numpy.clip( figWarped * 255, 0, 255 ).astype('uint8')
+  figWarped = stack_images( figWarped )
+  cv2.imshow( "w", figWarped )
+
+  print(warped_A.shape)
+  print(target_A.shape)
+
+  from model import autoencoder_A
+  from model import autoencoder_B
+
+
+  from model import encoder, decoder_A, decoder_B
+
+
+  try:
+      encoder  .load_weights( "models/encoder.h5"   )
+      decoder_A.load_weights( "models/decoder_A.h5" )
+      decoder_B.load_weights( "models/decoder_B.h5" )
+  except:
+      pass
+
+  def save_model_weights():
+      encoder  .save_weights( "models/encoder.h5"   )
+      decoder_A.save_weights( "models/decoder_A.h5" )
+      decoder_B.save_weights( "models/decoder_B.h5" ) 
+      print( "\nSave model weights" )
+
+
+
+  while 1:
+    pbar = tqdm(range(1000000))
+    for epoch in pbar:
+
+      
+        warped_A, target_A, mask_A = get_training_data( images_A,  landmarks_A,landmarks_B, batch_size )
+        warped_B, target_B, mask_B = get_training_data( images_B, landmarks_B,landmarks_A, batch_size )
+
+      
+        omask = numpy.ones((target_A.shape[0],64,64,1),float)
+
+        loss_A = autoencoder_A.train_on_batch([warped_A,mask_A], [target_A,mask_A])
+        loss_B = autoencoder_B.train_on_batch([warped_B,mask_B], [target_B,mask_B])
+
+ 
+        pbar.set_description("Loss A [{}] Loss B [{}]".format(loss_A,loss_B))
+
+
+        if epoch % 100 == 0:
+          save_model_weights()
+          test_A = target_A[0:8,:,:,:3]
+          test_B = target_B[0:8,:,:,:3]
+
+        figWarped = numpy.stack([warped_A[:6],warped_B[:6]],axis=0 )
+        figWarped = numpy.clip( figWarped * 255, 0, 255 ).astype('uint8')
+        figWarped = stack_images( figWarped )
+        cv2.imshow( "w", figWarped )
+
+        
+        zmask = numpy.zeros((test_A.shape[0],64,64,1),float)
+
+        pred_a_a,pred_a_a_m = autoencoder_A.predict([test_A,zmask])
+        pred_b_a,pred_b_a_m = autoencoder_B.predict([test_A,zmask])
+
+        pred_a_b,pred_a_b_m = autoencoder_A.predict([test_B,zmask])
+        pred_b_b,pred_b_b_m = autoencoder_B.predict([test_B,zmask])
+
+        pred_a_a = pred_a_a[0:18,:,:,:3]
+        pred_a_b = pred_a_b[0:18,:,:,:3]
+        pred_b_a = pred_b_a[0:18,:,:,:3]
+        pred_b_b = pred_b_b[0:18,:,:,:3]
+
+        figure_A = numpy.stack([
+            test_A,
+            pred_a_a,
+            pred_b_a,
+            ], axis=1 )
+        figure_B = numpy.stack([
+            test_B,
+            pred_b_b,
+            pred_a_b,
+            ], axis=1 )
+
+
+        figure = numpy.concatenate( [ figure_A, figure_B ], axis=0 )
+        figure = figure.reshape( (4,4) + figure.shape[1:] )
+        figure = stack_images( figure )
+
+        figure = numpy.clip( figure * 255, 0, 255 ).astype('uint8')
+
+
+        cv2.imshow( "p", figure )
+    
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            save_model_weights()
+            exit()
+
---
+++ training_data.py
@@ -0,0 +1,69 @@
+import numpy
+import queue
+import threading
+from image_augmentation import random_transform
+from image_augmentation import random_warp,random_warp_src_dest
+import cv2
+
+
+random_transform_args = {
+    'rotation_range': 10,
+    'zoom_range': 0.05,
+    'shift_range': 0.05,
+    'random_flip': 0.0,
+    }
+
+
+
+def async_get_training_data( images, srcPoints, dstPoints, batch_size,workerqueue ):
+
+
+    while 1:
+      indices = numpy.random.choice(range(0,images.shape[0]),size=batch_size,replace=False)
+      for i,index in enumerate(indices):
+          image = images[index]
+          image = random_transform( image, **random_transform_args )
+
+          closest = ( numpy.mean(numpy.square(srcPoints[index]-dstPoints),axis=(1,2)) ).argsort()[2:10]
+          closest = numpy.random.choice(closest)
+          warped_img, target_img, mask_image = random_warp_src_dest( image,srcPoints[index],dstPoints[ closest ] )
+ 
+          if numpy.random.random() < 0.5:
+            warped_img = warped_img[:,::-1]
+            target_img = target_img[:,::-1]
+            mask_image = mask_image[:,::-1]
+
+
+          if i == 0:
+              warped_images = numpy.empty( (batch_size,) + warped_img.shape, warped_img.dtype )
+              target_images = numpy.empty( (batch_size,) + target_img.shape, warped_img.dtype )
+              mask_images = numpy.empty( (batch_size,)   + mask_image.shape, mask_image.dtype )
+
+          warped_images[i] = warped_img
+          target_images[i] = target_img
+          mask_images[i]   = mask_image
+
+
+      workerqueue.put( (warped_images, target_images , mask_images ) )
+
+
+queues = {
+  
+}
+t=None
+q=None
+cycle=0
+def get_training_data( images , srcPoints, dstPoints, batch_size ):
+  global cycle
+  cycle+=1
+  key = (id(images),batch_size,cycle%3)
+  if key in queues:
+    t,q = queues.get(key)
+  else:
+    print('New Image thread start',key)
+    q = queue.Queue(maxsize=20)
+    t = threading.Thread(target=async_get_training_data,args=(images, srcPoints, dstPoints, batch_size,q))
+    t.daemon = True
+    t.start()
+    queues[key]=(t,q)
+  return q.get()
